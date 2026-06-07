5bdb61eb42bfc27ea9917fe59e8553d013458c80
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-06-07 21:46:11 +0530

    Some pattern printing questions solved !

--- patternprinting.js
+++ patternprinting.js
@@ -1,15 +1,53 @@
-console.log("*")
-console.log("***")
-console.log("*****")
-console.log("*******")
-console.log("*********")
+// console.log("*")
+// console.log("***")
+// console.log("*****")
+// console.log("*******")
+// console.log("*********")
 
-for(let r=1;r<=5;r++)
-{
-    let row=""
-    for(let s=1;s<=r;s++)
-    {
-        row+="*"
-    }
-    console.log(row)
-}
+// for (let r = 1; r <= 5; r++) {
+//     let star = ""
+//     let row = 0
+//     if (row <= 5) {
+//         for (let c = 1; c <= r + row; c++) {
+//             star += "*"
+//             row++
+//         }
+//         console.log(star)
+//     }
+// }
+
+// process.stdout.write("hello ")
+
+// Number printing
+// for (let r = 1; r <= 5; r++) 
+// {
+//     let sum=0
+//     for (let n = 1; n <= r; n++) 
+//     {
+//         sum+=1
+//        process.stdout.write(`${sum}`)
+//     }
+//     console.log()
+// }
+
+// Character printing 
+// for (let r = 1; r <= 5; r++) 
+// {
+//     let ascii=65
+//     for (let n = 1; n <= r; n++) 
+//     {
+//         process.stdout.write(`${String.fromCharCode(ascii)}`)
+//         ascii+=1
+//     }
+//     console.log()
+// }
+
+// inverted right angle triangle
+// for(let r=1;r<=5;r++)
+// {
+//     for(let c=6-r;c>=1;c--)
+//     {
+//         process.stdout.write("* ")
+//     }
+//     console.log()
+// }
