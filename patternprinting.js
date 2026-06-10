7cdf02ffb10ca114909873e86eeea40109cc8aed
Nirbhaykumarstp <nirbhaykumarstp59@gmail.com>
2026-06-10 21:02:35 +0530

    New row variable added!

--- patternprinting.js
+++ patternprinting.js
@@ -19,10 +19,11 @@
 // process.stdout.write("hello ")
 
 // Number printing
-// for (let r = 1; r <= 5; r++) 
+// let row=5
+// for (let r = 1; r <= row; r++) 
 // {
 //     let sum=0
-//     for (let n = 1; n <= r; n++) 
+//     for (let c = 1; c <= r; c++) 
 //     {
 //         sum+=1
 //        process.stdout.write(`${sum}`)
@@ -30,11 +31,12 @@
 //     console.log()
 // }
 
-// Character printing 
+// Character printing
+// let row=5 
 // for (let r = 1; r <= 5; r++) 
 // {
 //     let ascii=65
-//     for (let n = 1; n <= r; n++) 
+//     for (let c = 1; c <= r; c++) 
 //     {
 //         process.stdout.write(`${String.fromCharCode(ascii)}`)
 //         ascii+=1
@@ -43,7 +45,8 @@
 // }
 
 // inverted right angle triangle
-// for(let r=1;r<=5;r++)
+//let row=5
+// for(let r=1;r<=row;r++)
 // {
 //     for(let s=5;s>=r;s--)
 //     {
@@ -53,42 +56,45 @@
 // }
 
 //left slanted right angle triangle --combo of inverted triangleand right angle traingle
-for(let r=1;r<=5;r++)
-{
-    for(let s=5;s>=r;s--)
-    {
-        process.stdout.write(" ")
-    }
-    for(let c=1;c<=r;c++)
-    {
-        process.stdout.write("*")
-    }
-    console.log()
-}
+// let row=5
+// for(let r=1;r<=row;r++)
+// {
+//     for(let s=5;s>=r;s--)
+//     {
+//         process.stdout.write(" ")
+//     }
+//     for(let c=1;c<=r;c++)
+//     {
+//         process.stdout.write("*")
+//     }
+//     console.log()
+// }
 
 //cross-- only works for odd no. only
-for(let r=1;r<=7;r++)
-{
-    for(let c=1;c<=7;c++)
-    {
-        if(r==c || r+c==8)
-        {
-            process.stdout.write("*")
-        }
-        else process.stdout.write(" ")
-    }
-    console.log()
-}
+// let row=9
+// for(let r=1;r<=row;r++)
+// {
+//     for(let c=1;c<=row;c++)
+//     {
+//         if(r==c || r+c==row+1)
+//         {
+//             process.stdout.write("*")
+//         }
+//         else process.stdout.write(" ")
+//     }
+//     console.log()
+// }
 
 //v shape
-for(let r=1;r<=5;r++)
-{
-    for(let c=1;c<=9;c++)
-    {
-        if(r==c || r+c==10)
-        {
-            process.stdout.write("*")
-        }else  process.stdout.write(" ")
-    }
-    console.log()
-}
+// let row=6
+// for(let r=1;r<=row;r++)
+// {
+//     for(let c=1;c<=(2*row)-1;c++)
+//     {
+//         if(r==c || r+c==2*row)
+//         {
+//             process.stdout.write("*")
+//         }else  process.stdout.write(" ")
+//     }
+//     console.log()
+// }
